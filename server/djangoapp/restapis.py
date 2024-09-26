c0cd0ce4fa641c5773e9535015723492436e4bd8
gitPrerakJain <prerakjain00@gmail.com>
2024-09-26 11:43:42 -0400

    add dynamic pages

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -18,7 +18,7 @@ def get_request(endpoint, **kwargs):
     if(kwargs):
         for key,value in kwargs.items():
             params=params+key+"="+value+"&"
-
+    print("BackendUrl: ", backend_url)
     request_url = backend_url+endpoint+"?"+params
 
     print("GET from {} ".format(request_url))
