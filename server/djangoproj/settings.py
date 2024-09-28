e8009225aa7b0e987f9e072aeb0e475f30aa9b65
gitPrerakJain <prerakjain00@gmail.com>
2024-09-28 23:44:33 +0530

    fixed lint errors

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -23,7 +23,8 @@ print(BASE_DIR)
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-ccow$tz_=9%dxu4(0%^(z%nx32#s@(zt9$ih@)5l54yny)wm-0"
+SECRET_KEY = """django-insecure-ccow$tz_
+=9%dxu4(0%^(z%nx32#s@(zt9$ih@)5l54yny)wm-0"""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -117,12 +118,9 @@ AUTH_PASSWORD_VALIDATORS = [
             "UserAttributeSimilarityValidator"
         )
     },
-    {"NAME": 
-     ("django.contrib.auth.password_validation." "MinimumLengthValidator")},
-    {"NAME": 
-     ("django.contrib.auth.password_validation." "CommonPasswordValidator")},
-    {"NAME": 
-     ("django.contrib.auth.password_validation." "NumericPasswordValidator")},
+    {"NAME": ("django.contrib.auth.password_validation." "MinimumLengthValidator")},
+    {"NAME": ("django.contrib.auth.password_validation." "CommonPasswordValidator")},
+    {"NAME": ("django.contrib.auth.password_validation." "NumericPasswordValidator")},
 ]
 
 
