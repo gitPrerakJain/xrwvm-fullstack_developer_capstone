092888e0d20f5d7818636fdc43a03d9966346de2
gitPrerakJain <prerakjain00@gmail.com>
2024-09-28 13:28:20 -0400

    getCars endpoint working

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,54 +1,30 @@
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
-
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
-
     # Other fields as needed
+
     def __str__(self):
         return self.name  # Return the name as the string representation
 
-
 class CarModel(models.Model):
-    car_make = models.ForeignKey(
-        CarMake, on_delete=models.CASCADE
-    )  # Many-to-One relationship
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
     name = models.CharField(max_length=100)
     CAR_TYPES = [
-        ("SEDAN", "Sedan"),
-        ("SUV", "SUV"),
-        ("WAGON", "Wagon"),
-        ("COUPE", "Coupe"),
-        ("ELECTRIC", "Electric"),
-        ("HATCHBACK", "Hatchback"),
-        ("HYBRID", "Hybrid"),
-        ("VAN", "Van"),
-        ("MINIVAN", "Minivan"),
-        ("CROSSOVER", "Crossover"),
-        ("SPORT", "Sport"),
-        ("LUXURY", "Luxury"),
+        ('SEDAN', 'Sedan'),
+        ('SUV', 'SUV'),
+        ('WAGON', 'Wagon'),
+        # Add more choices as required
     ]
-    type = models.CharField(
-        max_length=10,
-        choices=CAR_TYPES,
-        default="SUV"
-    )
-    year = models.IntegerField(
-        default=2023,
+    type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
+    year = models.IntegerField(default=2024,
         validators=[
-            MaxValueValidator(2023),
+            MaxValueValidator(2025),
             MinValueValidator(2015)
-        ]
-    )
-    mileage = models.IntegerField(
-        default=0,
-        validators=[
-            MinValueValidator(0),
-            MaxValueValidator(200000)
-        ]
-    )
+        ])
+    # Other fields as needed
 
     def __str__(self):
         return self.name  # Return the name as the string representation
--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -11,8 +11,6 @@ urlpatterns = [
     path(route="login", view=views.login_user, name="login"),
     # path for logout
     path(route="logout", view=views.logout_request, name="logout"),
-    # path for get_cars
-    path(route="get_cars", view=views.get_cars, name="getcars"),
     # path for dealer reviews view
     path(route="get_dealers/", view=views.get_dealerships, name="get_dealers"),
     path(
@@ -32,4 +30,6 @@ urlpatterns = [
         name="dealer_details",
     ),
     path(route="add_review", view=views.add_review, name="add_review"),
+    # path for get_cars
+    path(route="get_cars/", view=views.get_cars, name ="getcars"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
