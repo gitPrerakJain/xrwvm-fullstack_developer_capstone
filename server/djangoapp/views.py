d17458e6034cfa32ed7b5df79e131b066aa61768
gitPrerakJain <prerakjain00@gmail.com>
2024-09-28 11:48:29 -0400

    module 4 functionality added

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -80,16 +80,14 @@ def registration(request):
 
 def get_cars(request):
     count = CarMake.objects.filter().count()
-    if count == 0:
+    print(count)
+    if(count == 0):
         initiate()
-    car_models = CarModel.objects.select_related("car_make")
+    car_models = CarModel.objects.select_related('car_make')
     cars = []
     for car_model in car_models:
-        cars.append({
-            "CarModel": car_model.name,
-            "CarMake": car_model.car_make.name
-        })
-    return JsonResponse({"CarModels": cars})
+        cars.append({"CarModel": car_model.name, "CarMake": car_model.car_make.name})
+    return JsonResponse({"CarModels":cars})
 
 
 # Update the `get_dealerships` render list
